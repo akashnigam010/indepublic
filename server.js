1b6a4d0aaded396fb2578d99688dbeed5966a51a
Akash Nigam <akashnigam010@gmail.com>
2017-12-25 19:19:31 +0530

    Temp server.js https changes

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const http = require('http');
+const https = require('https');
 var express = require('express');
 var app = express();
 var path = require('path');
@@ -49,7 +51,7 @@ app.post('/subscribe', function (req, res) {
 	    res.send(JSON.stringify({"status" : true }));
 	  }
 	});
-})
+});
 
 app.post('/message', function (req, res) {
 	var name = req.body.name;
@@ -74,8 +76,34 @@ app.post('/message', function (req, res) {
 	    res.send(JSON.stringify({"status" : true }));
 	  }
 	});
-})
+});
 
-var server = app.listen(8080, function () {
-  console.log("Node server up and running on port: " + server.address().port);
-})
+const PROD = false;
+//const redirectHttps = require('redirect-https');
+
+const lex = require('greenlock-express').create({
+  server: PROD ? 'https://acme-v01.api.letsencrypt.org/directory' : 'staging',
+ 
+  approveDomains: (opts, certs, cb) => {
+    if (certs) {
+      opts.domains = ['localhost']
+    } else { 
+      opts.email = 'akashnigam020@gmail.com'
+      opts.agreeTos = true;
+    }
+    cb(null, { options: opts, certs: certs });
+  }
+});
+const middlewareWrapper = lex.middleware;
+const redirectHttps = require('redirect-https');
+
+var httpServer = http.createServer(lex.middleware(redirectHttps()));
+var httpsServer = https.createServer(lex.httpsOptions, middlewareWrapper(app));
+
+httpServer.listen(80, function () {
+  console.log("Node server up and running on port: " + httpServer.address().port);
+});
+
+httpsServer.listen(443, function () {
+  console.log("Node server up and running on port: " + httpsServer.address().port);
+});
